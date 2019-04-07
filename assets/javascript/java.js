dc4742529ecbfc0f189c5c5112bfe6a703f38efa
Non Dewman <Dnewman1934@yahoo.com>
2019-04-07 15:10:13 -0500

    firebase workin, oo

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -1,1 +1,32 @@
 console.log('aye')
+var busNum;
+var busDest;
+var initialLeave;
+var trainFrequency;
+var config = {
+    apiKey: "AIzaSyAgCT-vlZgqogA2vjv7qGjnE8agod2FDTY",
+    authDomain: "nuclassproj.firebaseapp.com",
+    databaseURL: "https://nuclassproj.firebaseio.com",
+    projectId: "nuclassproj",
+    storageBucket: "nuclassproj.appspot.com",
+    messagingSenderId: "180178704154"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
+console.log(database);
+$('#add-bus').on('click', function (event) {
+
+    event.preventDefault();
+    busNum = $('#busNum').val().trim();
+    busDest = $('#trainDest').val().trim();
+    busLeave = $('#initialDep').val().trim();
+    busFreq = $('#frequency').val().trim();
+    database.ref().push({
+        idNumber: busNum,
+        destination: busDest,
+        initialLeave: busLeave,
+        trainFrequency: busFreq,
+    }, function (errorObject) {
+        console.log(errorObject);
+    })
+});
